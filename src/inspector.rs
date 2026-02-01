592cb72a45d07db171b0478615b58bd5bbf0cf14
Crash <1903291+hex22a@users.noreply.github.com>
2026-02-01 23:19:47 +0400

    feat!: Allow multiple CIDRs. Add JSON output versioning. Prettify output for JSON. Introduce headless mode for table output

--- src/inspector.rs
+++ src/inspector.rs
@@ -1,8 +1,11 @@
 use serde::Serialize;
 use tabled::{Table, Tabled};
-use tabled::settings::Style;
+use tabled::settings::{Remove, Style};
+use tabled::settings::object::Rows;
 
 pub mod ipv4;
+
+const JSON_OUTPUT_VERSION: u8 = 1;
 
 #[derive(Debug, PartialEq, Eq, Serialize, Tabled)]
 pub(crate) struct InspectionResult {
@@ -13,10 +16,29 @@ pub(crate) struct InspectionResult {
     pub(crate) broadcast: String,
 }
 
-pub fn print_table(result: &InspectionResult) {
-    let mut binding = Table::new(vec![result]);
-    let table = binding
-        .with(Style::blank());
+#[derive(Serialize)]
+struct JsonOutput<'a> {
+    version: u8,
+    data: &'a Vec<InspectionResult>
+}
+
+pub fn print_json(inspection_results: &Vec<InspectionResult>, pretty: &bool) {
+    let json_output = JsonOutput {
+        version: JSON_OUTPUT_VERSION,
+        data: inspection_results,
+    };
+    match pretty {
+        true => println!("{}", serde_json::to_string_pretty(&json_output).unwrap()),
+        false => println!("{}", serde_json::to_string(&json_output).unwrap()),
+    }
+}
+
+pub fn print_table(results: &Vec<InspectionResult>, headless: &bool) {
+    let mut table = Table::new(results);
+    (&mut table).with(Style::blank());
+    if *headless {
+        (&mut table).with(Remove::row(Rows::first()));
+    }
 
     println!("{table}");
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod ip;
 mod inspector;
 
 use clap::{Parser, ValueEnum};
+use crate::inspector::InspectionResult;
 use crate::inspector::ipv4::Inspectable;
 use crate::ip::ipv4::Ipv4Cidr;
 
@@ -14,29 +15,43 @@ enum OutputFormat {
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// CIDR e.g. 10.122.33.129/24
-    cidr: String,
+    /// One or more CIDRs e.g. 10.122.33.129/24
+    cidrs: Vec<String>,
 
     #[arg (short, long, default_value = "table")]
     format: OutputFormat,
+
+    /// Prettify the JSON output (only for --format=json)
+    #[arg (short, long)]
+    pretty: bool,
+
+    /// Print table without header (only for --format=table)
+    #[arg (short='H', long)]
+    headless: bool,
 }
 
 fn main() {
     let args = Args::parse();
-    let cidr = match args.cidr.parse::<Ipv4Cidr>() {
-        Ok(cidr) => cidr,
-        Err(err) => {
-            eprintln!("Invalid CIDR '{}': {:?}", args.cidr, err);
-            std::process::exit(1);
+
+    let cidrs: Vec<Ipv4Cidr> = args.cidrs.iter().map(|cidr| {
+        match cidr.parse::<Ipv4Cidr>() {
+            Ok(cidr) => cidr,
+            Err(err ) => {
+                eprintln!("Invalid CIDR '{}': {:?}", cidr, err);
+                std::process::exit(1);
+            }
         }
-    };
-    let inspection_result = cidr.inspect();
+    }).collect::<Vec<Ipv4Cidr>>();
+
+    let inspection_results: Vec<InspectionResult> = cidrs.iter()
+        .map(|cidr| cidr.inspect()).collect();
+
     match args.format {
         OutputFormat::Json => {
-            println!("{}", serde_json::to_string(&inspection_result).unwrap());
+            inspector::print_json(&inspection_results, &args.pretty);
         },
         OutputFormat::Table => {
-            inspector::print_table(&inspection_result);
+            inspector::print_table(&inspection_results, &args.headless);
         }
     }
 }
